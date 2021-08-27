0c7d03d40f59b966f86946b67ad64898828079ee
Concerto-hub <user.mac@concertosoft.com>
2021-08-27 19:53:21 +0530

    initial commit

---
+++ Example/Pods/Target Support Files/Pods-UrwayPayments_Example/Pods-UrwayPayments_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UrwayPayments_Example : NSObject
+@end
+@implementation PodsDummy_Pods_UrwayPayments_Example
+@end
---
+++ Example/Pods/Target Support Files/UrwayPayments/UrwayPayments-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UrwayPayments : NSObject
+@end
+@implementation PodsDummy_UrwayPayments
+@end
